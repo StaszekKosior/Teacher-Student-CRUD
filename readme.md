132ba36459bc3a0a8306e0e55a43513791eb0990
StaszekKosior <kosior.stanislaw@gmail.com>
2022-11-24 10:05:27 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Student/Teacher CRUD
+
+### General info
+
+Project made as a part of recruitment process
+
+### Technologies
+- Java
+- Spring Boot
+- Spring MVC
+- Hibernate
+- JSP
+
---
+++ src/test/java/com/teacherstudentcrud/TeacherStudentCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacherstudentcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherStudentCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
